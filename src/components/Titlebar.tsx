c82290d8dd00186b770431cd50c243ecdeca3601
Seto Elkahfi <hej@setoelkahfi.se>
2025-06-21 23:36:39 +0200

    add radix-ui

--- src/components/Titlebar.tsx
+++ src/components/Titlebar.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import { getCurrentWebviewWindow } from "@tauri-apps/api/webviewWindow";
-import { platform } from '@tauri-apps/plugin-os';
+import { platform } from "@tauri-apps/plugin-os";
 const currentPlatform = platform();
 
 // Prints "windows" to the console
-const appWindow = getCurrentWebviewWindow()
+const appWindow = getCurrentWebviewWindow();
 
 export default function Titlebar() {
   const [isScaleup, setScaleup] = useState(false);
@@ -12,34 +12,48 @@ export default function Titlebar() {
   const onScaleup = () => {
     appWindow.toggleMaximize();
     setScaleup(true);
-  }
+  };
 
   const onScaledown = () => {
     appWindow.toggleMaximize();
     setScaleup(false);
-  }
+  };
 
   const isMobile = () => {
     if (currentPlatform == "ios" || currentPlatform == "android") {
       return true;
     }
-    return false
-  }
+    return false;
+  };
 
   const onClose = () => appWindow.close();
 
-  return <div id="titlebar" data-tauri-drag-region>
-    <div className="flex items-center gap-1 5 pl-2">
-      <img src="/tauri.svg" style={{ width: 10 }} alt="" />
-      <span className="text-xs">MoVibe</span>
+  const scaleUpDown = isScaleup ? (
+    <i className="titlebar-icon ri-file-copy-line" onClick={onScaledown}></i>
+  ) : (
+    <i onClick={onScaleup} className="titlebar-icon ri-stop-line"></i>
+  );
+
+  return (
+    <div id="titlebar" data-tauri-drag-region>
+      <div className="flex items-center gap-1 5 pl-2">
+        <img src="/tauri.svg" style={{ width: 10 }} alt="" />
+        <span className="text-lg">MoVibe</span>
+      </div>
+      {!isMobile && (
+        <div className="titlebar-actions">
+          <i
+            className="titlebar-icon ri-subtract-line"
+            onClick={onMinimize}
+          ></i>
+          {scaleUpDown}
+          <i
+            id="ttb-close"
+            className="titlebar-icon ri-close-fill"
+            onClick={onClose}
+          ></i>
+        </div>
+      )}
     </div>
-
-    <div className="titlebar-actions">
-      <i className="titlebar-icon ri-subtract-line" onClick={onMinimize}></i>
-
-      {isScaleup ? <i className="titlebar-icon ri-file-copy-line" onClick={onScaledown}></i> : <i onClick={onScaleup} className="titlebar-icon ri-stop-line"></i>}
-
-      <i id="ttb-close" className="titlebar-icon ri-close-fill" onClick={onClose}></i>
-    </div>
-  </div>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "remixicon/fonts/remixicon.css"
+import "remixicon/fonts/remixicon.css";
 import "./style.css";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Theme>
+      <App />
+    </Theme>
+  </React.StrictMode>,
 );
-
